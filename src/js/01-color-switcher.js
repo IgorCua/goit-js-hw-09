aa0b0c585dfce26def6a3404f75c17b36188cb9b
IhorC <igorchua@gmail.com>
2023-03-03 14:28:05 +0200

    task 01 added

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+startBtn.addEventListener('click', colorsWitcher);
+stopBtn.addEventListener('click', stopInterval);
+
+function colorsWitcher(event){
+    startBtn.disabled = true;
+    timer = setInterval(()=>{
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function stopInterval(event){
+    clearInterval(timer);
+    startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
