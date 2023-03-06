a039ab1bf857c3817dd8f9a55431c9171ab47786
IgorCua <igorchua@gmail.com>
2023-03-06 15:29:52 +0000

    Deploying to gh-pages from @ IgorCua/goit-js-hw-09@b6fd35f3d7fda64d2fd223dc5cdd5f6231d87955 🚀

--- 03-promises.e922d54b.js
+++
@@ -1,2 +0,0 @@
-function e(e,t){return new Promise(((o,n)=>{setTimeout((()=>{Math.random()>.3?o({position:e,delay:t}):n({position:e,delay:t})}),t)}))}document.querySelector(".form").addEventListener("submit",(function(t){t.preventDefault();const o=t.target.elements;let n=+o.delay.value;for(let t=1;t<=o.amount.value;t++)1!==t&&(n+=+o.step.value),e(t,n).then((({position:e,delay:t})=>{console.log(`✅ Fulfilled promise ${e} in ${t}ms`)})).catch((({position:e,delay:t})=>{console.log(`❌ Rejected promise ${e} in ${t}ms`)}))}));
-//# sourceMappingURL=03-promises.e922d54b.js.map
