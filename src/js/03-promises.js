fea2e5bc5c408e1d220a339b104b4a89169f1720
IhorC <igorchua@gmail.com>
2023-03-06 15:33:43 +0200

    task 03 fixed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -23,13 +23,6 @@ function submit(event){
   console.log(event.target.elements.delay.value);
   console.log(event.target.elements.step.value);
 }
-// createPromise(2, 1000)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
 
 function formData(event){
   event.preventDefault();
@@ -38,7 +31,7 @@ function formData(event){
 
   for(let i = 1; i <= elements.amount.value; i++){
     if(i !== 1) miliseconds += +elements.step.value, 10;
-    console.log(miliseconds)
+
     createPromise(i, miliseconds)
     .then(({ position, delay }) => {
       console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
